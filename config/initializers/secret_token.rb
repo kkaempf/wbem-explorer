2764b3b4faed636be039264b4de3b38153bdf6a1
Klaus Kämpf <kkaempf@suse.de>
2011-10-28 16:14:27 +0200

    Initial Rails3 template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WbemExplorer::Application.config.secret_token = 'e4de2f7a1c7e53eb58b8cfc519d5257a044335aa58f9225652603f176ec1097117486a31deb1baf1943b6e9fe69e46eb5d8e3314dabd8c9fa0773af70f0dad90'
