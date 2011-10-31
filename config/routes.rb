9d71040221bed3d74e82c285f71d1cc68099d9ab
Klaus Kämpf <kkaempf@suse.de>
2011-10-31 11:03:54 +0100

    Add Hosts and Users as resource

--- config/routes.rb
+++ config/routes.rb
@@ -50,6 +50,9 @@ WbemExplorer::Application.routes.draw do
   # just remember to delete public/index.html.
   root :to => 'start#index'
 
+  map.resources :hosts
+  map.resources :users
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
