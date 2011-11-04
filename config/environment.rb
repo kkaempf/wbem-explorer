82165199b94ba0f8b574e16177746c0855429d27
Klaus Kämpf <kkaempf@suse.de>
2011-11-04 21:20:40 +0100

    Complete switch to web-app-theme

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 WbemExplorer::Application.initialize!
+
+# https://github.com/bryanbibat/pro-template-app-31/blob/d94cb74f406e79e6f2d93715a72333d33f93684e/config/environment.rb
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  if html_tag =~ /<label/
+    %|<div class="fieldWithErrors">#{html_tag} <span class="error">#{[instance.error_message].join(', ')}</span></div>|.html_safe
+  else
+    html_tag
+  end
+end
