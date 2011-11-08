a21701efb227a4a46f1f32d57f7c6ec3edf691c0
Klaus Kämpf <kkaempf@suse.de>
2011-11-08 16:36:40 +0100

    Remove connection stuff from home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,7 @@
 class HomeController < ApplicationController
-  require "lib/wbem_client.rb"
   
   # initial view, builds up main window with
   # sub-windows for Host, Function, View and Detail
   def index
-    if session[:url]
-      client,options = WbemClient.connect session[:url]
-      if client
-	result = client.identify( options )
-	if result
-	  @identify = { :product_vendor => result.ProductVendor, :product_version => result.ProductVersion, :protocol_version => result.ProtocolVersion }
-	else
-	  @identify = { :error => "Can't identify #{session[:connection]}" }
-	end
-      end
-    end
   end
 end
