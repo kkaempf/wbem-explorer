78e2c8ed2db37e0a7a13b91f1948e5d60d65a07e
Klaus Kämpf <kkaempf@suse.de>
2011-11-16 20:35:11 +0100

    Self reference CimClass.parent

--- app/models/cim_class.rb
+++ app/models/cim_class.rb
@@ -1,6 +1,7 @@
 class CimClass < ActiveRecord::Base
   belongs_to :cim_schema
-  belongs_to :cim_class # parent
+  # self reference for parent
+  belongs_to :parent, :class_name => "CimClass", :foreign_key => "parent_id"
   validates_uniqueness_of :name
   paginates_per 10
 
