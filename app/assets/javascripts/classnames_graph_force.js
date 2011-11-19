c13094ba301f2ca88f5c658d371fd790e6a11380
Klaus Kämpf <kkaempf@suse.de>
2011-11-19 12:35:39 +0100

    Put the force field graph aside, start with Tree view graph (wip)

--- app/assets/javascripts/classnames_graph_force.js
+++ app/assets/javascripts/classnames_graph_force.js
@@ -10,21 +10,22 @@ $(function(){
 
   var names = [];
   var nodes = [];
+  var links = [];
 
   $('#classnames_data > div').each(function(index) {
     n = $(this).attr('id');
     parent = $(this).attr('parent');
     names[n] = nodes.length;
     console.log("Node " + names[n] + ": " + n + " -> " + parent);
-    nodes.push({name: n, children: [], parent: parent});
+    nodes.push({name: n, parent: parent});
   });
-  console.log("Extracting children of " + nodes.length + " nodes");
+  console.log("Extracting links of " + nodes.length + " nodes");
   var i;
   for (i=0; i < nodes.length; i += 1) {
     var node = nodes[i];
-    var node_id = names[node.name];
+    var name_id = names[node.name];
     var parent_id = names[node.parent];
-    console.log("Node " + i + ": " + node.name + "("+node_id+") -> " + node.parent + "(" + parent_id + ")");
+    console.log("Node " + i + ": " + node.name + "("+name_id+") -> " + node.parent + "(" + parent_id + ")");
     if (parent_id !== undefined) {
       link = {source: names[node.name], target: names[node.parent], value: 5};
       console.log("Link " + links.length + ": " + link.source + " -> " + link.target);
@@ -34,16 +35,18 @@ $(function(){
   console.log(nodes.length + " nodes");
   console.log(links.length + " links");
   console.log("Available symbol types");
-  var tree = d3.layout.tree()
+  var force = d3.layout.force()
+    .charge(-800)
+    .gravity(0.1)
+    .linkStrength(1)
+    .linkDistance(30)
     .nodes(nodes)
+    .links(links)
     .size([w, h])
     .start();
-
-  var diagonal = d3.svg.diagonal()
-    .projection(function(d) { return [d.y, d.x]; });
-
+    
   var link = vis.selectAll("line.link")
-      .data(tree.links(links))
+      .data(links)
       .enter().append("svg:line")
       .attr("class", "link")
       .style("stroke-width", 4)
