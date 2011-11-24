2559182d9389f553aaf53f7c7671f124f65865ab
Klaus Kämpf <kkaempf@suse.de>
2011-11-24 15:33:38 +0100

    Force utf-8 encoding

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# http://stackoverflow.com/questions/4188677/ruby-on-rails-3-incompatible-character-encodings-utf-8-and-ascii-8bit-with-i18
+Encoding.default_external = Encoding::UTF_8
+Encoding.default_internal = Encoding::UTF_8
+
 # Initialize the rails application
 WbemExplorer::Application.initialize!
 
