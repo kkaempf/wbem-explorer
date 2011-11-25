cb63ae1efd48afd710aa01a19495eb2fca4f454a
Klaus Kämpf <kkaempf@suse.de>
2011-11-25 14:49:15 +0100

    Don't paginate tree and graph views

--- app/controllers/classnames_controller.rb
+++ app/controllers/classnames_controller.rb
@@ -7,7 +7,12 @@ class ClassnamesController < ApplicationController
     c = WbemClient.connect url
     @ns = params[:ns]
     @mode = params[:mode] || "list"
+    @conn = Connection.find(session[:connection])
+    @title = "#{@conn.name}: Class names for namespace #{@ns}"
     # Use Kaminari pagination with an array
-    @classnames = Kaminari.paginate_array(c.classnames(@ns, true).sort).page(params[:page]).per(20)
+    @classnames = c.classnames(@ns, true).sort
+    if @mode == "list"
+      @classnames = Kaminari.paginate_array(@classnames).page(params[:page]).per(20)
+    end
   end
 end
