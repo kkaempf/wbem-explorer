93f72d7699da37d0390bef06b7d130372dc86a4c
Klaus Kämpf <kkaempf@suse.de>
2011-11-28 15:43:02 +0100

    Decrease logging

--- app/assets/javascripts/cim_classes_tree_tree.js
+++ app/assets/javascripts/cim_classes_tree_tree.js
@@ -5,7 +5,7 @@ $(function(){
   $("#classes_tree").dynatree({
     onClick: function(node, event) {
       var e = node.getEventTargetType(event);
-      console.log("CimClass onClick " + e + ": " + node.data.title);
+//      console.log("CimClass onClick " + e + ": " + node.data.title);
       if (e === 'title') {
         window.location.href = "/cim_classes/"+node.data.title;
       }
