b8864957d720beafcc5881b6a9b32ccb8f32fc04
Klaus Kämpf <kkaempf@suse.de>
2011-11-29 14:30:03 +0100

    'Encoding' is Ruby 1.9 only

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,10 @@
 require File.expand_path('../application', __FILE__)
 
 # http://stackoverflow.com/questions/4188677/ruby-on-rails-3-incompatible-character-encodings-utf-8-and-ascii-8bit-with-i18
-Encoding.default_external = Encoding::UTF_8
-Encoding.default_internal = Encoding::UTF_8
+if Module.const_defined? "Encoding"
+  Encoding.default_external = Encoding::UTF_8
+  Encoding.default_internal = Encoding::UTF_8
+end
 
 # Initialize the rails application
 WbemExplorer::Application.initialize!
