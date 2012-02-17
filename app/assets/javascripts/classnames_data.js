34725eeb41ea27ac3b32d1668b5eb5ffc8d713e4
Klaus Kämpf <kkaempf@suse.de>
2012-02-17 13:24:19 +0100

    Add indented (collapsible) tree view

--- app/assets/javascripts/classnames_data.js
+++
@@ -1,7 +0,0 @@
-// Provide data connector information for all cim classes
-//
-
-console.log ("classnames_data.js");
-
-var data_prefix = function() { return "cim_classes"; };
-var data_url = function() { return data_prefix() + "/data"; };
