bf15d2b5faca314d5a9282eb904d2d7d6663bab1
Klaus Kämpf <kkaempf@suse.de>
2012-03-13 16:07:34 +0100

    Drop sfcc reference, use wbem

--- app/controllers/instances_controller.rb
+++ app/controllers/instances_controller.rb
@@ -5,10 +5,9 @@ class InstancesController < ApplicationController
     klass = CimClass.find(@class)
     @title = "Instances of class #{klass.name}"
     @ns = params[:ns] || "root/cimv2"
-    @op = Sfcc::Cim::ObjectPath.new(@ns,klass.name)
     @conn = Connection.find(session[:connection])
     client = @conn.connect
-    @instances = client.instance_names(@op)
+    @instances = client.instance_names(@ns,klass.name)
     @instances = Kaminari.paginate_array(@instances).page(params[:page]||1).per(20)
   end
   def show
