8fa3f2dfca3f7a363d388e1bdc5c00e09cc34410
Klaus Kämpf <kkaempf@suse.de>
2015-02-24 10:17:22 +0100

    precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( wbem_explorer.css wbem_explorer.js debug.js tooltip.js )
+Rails.application.config.assets.precompile += %w( classes_tree_tree.js )
+Rails.application.config.assets.precompile += %w( classes_graph_force.js )
+Rails.application.config.assets.precompile += %w( classes_graph_tree.js )
+Rails.application.config.assets.precompile += %w( classes_graph_indented.js )
+Rails.application.config.assets.precompile += %w( indented.css )
